a7e3d54d2bc8974357766cf216ece60c107a51a8
PhilipZubel <philip.zubel@glasgow.ac.uk>
2021-09-10 16:47:06 +0100

    fixed deployment

--- App.js
+++ App.js
@@ -1,12 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  Alert
+} from "react-native";
+
+import db from "./services/db";
+import validateIPAddress from "./services/validateIP";
+import Items from "./components/Items";
+
+// console.clear();
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    let mounted = true;
+
+    fetch();
+    create_table();
+
+    return () => (mounted = false);
+  }, []);
+
+  function fetch() {
+    db.fetch()
+      .then((rows) => setData(rows))
+      .catch((error) => Alert.alert(error));
+  }
+
+  function add() {
+
+    let isValid = validateIPAddress(value);
+
+    if(!isValid){
+      Alert.alert(
+        "IP address '"+value+"' is not valid",
+        "example: 123.45.67.89:9999",
+      )
+      setValue("")
+    }else{
+      db.add(value)
+      .then(() => {
+        setValue("");
+        fetch();
+      })
+      .catch((error) => Alert.alert(error));
+      }
+    }
+
+    
+
+  function remove(id) {
+    db.delete(id)
+      .then(fetch)
+      .catch((error) => Alert.alert(error));
+  }
+
+  function create_table() {
+    async function check_table() {
+        db.create_table()
+          .catch((error) => Alert.alert(error));
+      }
+
+    check_table();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <KeyboardAvoidingView behavior="position">
+        <View style={styles.header}>
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>CSPR servers:</Text>
+          <Text style={{ fontSize: 30, fontWeight: "bold", color: "#841584", marginRight: 14 }}>
+            {data.length}
+          </Text>
+        </View>
+        {data &&
+          data.map(({ id, name }) => (
+            <Items key={id} id={id} IP={name} onDelete={remove} />
+          ))}
+
+        <TextInput
+          value={value}
+          style={styles.input}
+          placeholder="Add IP address here..."
+          onChangeText={setValue}
+        />
+        <Button title="Add" color="#841584" onPress={add} />
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +100,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    paddingTop: 90,
+    backgroundColor: "#fff",
   },
+  input: {
+    padding: 10,
+    marginTop: 20,
+    borderColor: "#B6C",
+    borderWidth: 2,
+    borderRadius: 4,
+    marginBottom: 10,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // marginBottom: 20,
+    paddingBottom: 20,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "#ccc",
+  },
+
 });
---
+++ components/Items.js
@@ -0,0 +1,100 @@
+import React, {useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, StyleSheet} from "react-native";
+
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const Item = ({ id, IP, onDelete }) => {
+    
+    const [status, setStatus] = useState(1);
+    // 0 - active
+    // 1 - not active
+
+    useEffect(() => {
+        checkStatus();
+      }, []);
+
+    const checkStatus = () => {
+        //const url = "http://78.159.84.140:8888/status";
+        const url = "http://"+IP+"/status";
+
+        fetch(url)
+        .then(response =>{
+            return response.json()
+        }).then(data => {
+            if(data.chainspec_name == "casper"){
+                console.log("return true")
+                return true;
+            }else{
+              return false;
+            }
+        }).catch(response => {
+          return false;
+        }).then(response => {
+          if(response === true){
+            setStatus(0); // active
+          }
+          else if(response === false){
+            setStatus(1);  // not active
+          }else{
+            setStatus(1);
+          }
+    
+        })
+        
+        return "hi";
+      }
+    
+      const icons =  [
+        {status:0, name: "checkcircleo", color: "#00ff00"},
+        {status:1, name: "closecircleo", color: "#ff0000"},
+        // {status:2, name: "questioncircleo", color: "#fad02C"},
+    ]
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.item}>
+        <Text style={styles.completeCircle}>
+            <Icon name={icons[status].name} size={30} color={icons[status].color} />
+        </Text>
+        <Text style={{ fontSize: 15 }}>{IP} </Text>
+      </View>
+      <TouchableOpacity activeOpacity={0.5} onPress={() => onDelete(id)}>
+        <Icon style={styles.delete} name="delete" size={30} color="#e33057"/>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingVertical: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  item: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    margin: 10,
+  },
+  badge: {
+    height: 10,
+    width: 10,
+    borderRadius: 100,
+    marginRight: 20,
+  },
+  completeCircle: {
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  delete : {
+    marginRight: 8,
+  }
+});
+
+export default Item;
